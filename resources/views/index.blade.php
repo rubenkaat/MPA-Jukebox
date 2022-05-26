4429277c148434a6e094b69bcc887daa367d5d93
rubenkaat <99034608@mydavinci.nl>
2022-05-26 11:57:18 +0200

    started laravel application
    created login system

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<head>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <h1>Jukebox</h1>
+    <div id="genrebox">
+        <h1>genre name</h1>
+        <a><div>IMAGE</div></a>
+    </div>
+</body>
